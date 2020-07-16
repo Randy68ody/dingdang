f2fda0bdf87258826e9379be9cf30b1ac42b6d1e
13703320391@163.com <FVictory98>
2020-07-16 16:17:21 +0800

    array_key_exists报错屏蔽

--- application/api/service/UserToken.php
+++ application/api/service/UserToken.php
@@ -26,7 +26,7 @@ class UserToken extends Token {
         if(empty($wxRes)){
             throw new Exception('微信内部错误，获取session_key和openID时异常');
         }else{
-            $loginFail = array_key_exists('errcode',$wxRes);
+            $loginFail = @array_key_exists('errcode',$wxRes);
             if($loginFail){
                 $this->processLoginError($wxRes);
             }else{
