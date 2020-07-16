34a787cdced2a6956795095f7d353331688f2c7e
13703320391@163.com <FVictory98>
2020-07-16 09:37:56 +0800

    wx appid和appsecret设置

--- application/extra/wx.php
+++ application/extra/wx.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'appid' => 'wxbdf70c2f0e1bbc34',
-    'app_secret' => 'bf095f8cbe735bbaef8beaa15ab67043',
+    'appid' => 'wx5a747e245952ed29',
+    'app_secret' => '487516f92e1b135c873f7110e4f7e60e',
     'login_url' => 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code',
 ];
