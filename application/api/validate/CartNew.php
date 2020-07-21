18df6d957ac059948614cd2fc3439493343dea19
13703320391@163.com <FVictory98>
2020-07-21 17:06:00 +0800

    数量相减

--- application/api/validate/CartNew.php
+++ application/api/validate/CartNew.php
@@ -4,6 +4,6 @@ namespace app\api\validate;
 class CartNew extends BaseValidate {
     protected $rule = [
         'food_id' => 'isPositiveInteger|isNotEmpty',
-        'num' => 'isPositiveInteger|isNotEmpty'
+        'num' => 'require|isNotEmpty'
     ];
 }
