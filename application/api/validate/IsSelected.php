ed25b08b60d23952c1d286fde9f6a095bd303353
13703320391@163.com <FVictory98>
2020-07-23 17:05:13 +0800

    店铺购物车商品是否被选中

--- application/api/validate/IsSelected.php
+++ application/api/validate/IsSelected.php
@@ -3,7 +3,6 @@ namespace app\api\validate;
 
 class IsSelected extends BaseValidate {
     protected $rule = [
-        'store_id'=>'require|isPositiveInteger',
         'id'=>'require|isNotEmpty',
         'select'=>'require|isNotEmpty'
     ];
